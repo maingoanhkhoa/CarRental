af4f91942b9457ce20cbda004303a5f990f1a71c
anhkhoamai02 <10421027@student.vgu.edu.vn>
2026-04-22 23:00:52 +0700

    home

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
-// const API_URL = 'http://localhost:5000/api';
-const API_URL = "http://192.168.13.128:5000/api";
+const API_URL = 'http://localhost:5000/api';
+// const API_URL = "http://192.168.1.232:5000/api";
 
 const api = {
     async getCars(params = {}) {
